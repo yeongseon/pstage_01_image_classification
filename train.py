4c2c4247da53c3ced49edfe0200bc799f23d0a33
MoMentum99 <kmc2048@gmail.com>
2021-03-02 23:04:50 +0900

    Fix to get_last_lr()

--- train.py
+++ train.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
             if (idx + 1) % train_log_interval == 0:
                 train_loss = loss_value / train_log_interval
                 train_acc = matches / batch_size / train_log_interval
-                current_lr = scheduler.get_lr()
+                current_lr = scheduler.get_last_lr()
                 print(
                     f"Epoch[{epoch}/{num_epochs}]({idx + 1}/{len(train_loader)}) || "
                     f"training loss {train_loss:4.4} || training accuracy {train_acc:4.2%} || lr {current_lr}"
