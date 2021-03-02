728ad36ca9408528898a558fc98da6eae809ac9d
MoMentum99 <kmc2048@gmail.com>
2021-03-02 19:55:38 +0900

    Update with MaskBaseModel

--- model.py
+++ model.py
@@ -9,17 +9,26 @@ class MaskBaseModel(nn.Module):
 
         self.net = self.build_layers(num_classes, pretrained)
 
-        if not pretrained:
-            self._initialize_weights()
+        self._initialize_weights(pretrained)
 
-        if not freeze:
+        if freeze:
             self._freeze_net()
 
-    def forward(self, x):
+    def build_layers(self, num_classes, pretrained):
         raise NotImplementedError
 
-    def _initialize_weights(self):
-        for m in self.modules():
+    def classifier_layers(self):
+        """ returns classifier layers to be referenced for weight freezing or weight initializing """
+
+        raise NotImplementedError
+
+    def forward(self, x):
+        return self.net(x)
+
+    def _initialize_weights(self, pretrained):
+        """ initialize all weights if not pretrained, otherwise initialize only classifier weights """
+        modules = self.modules() if not pretrained else self.classifier_layers()
+        for m in modules:
             if isinstance(m, nn.Conv2d):
                 init.xavier_uniform_(m.weight.data)
                 if m.bias is not None:
@@ -32,34 +41,20 @@ class MaskBaseModel(nn.Module):
                 m.bias.data.zero_()
 
     def _freeze_net(self):
+        """ freeze all layers excepting classifier layers """
         for param in self.net.parameters():
             param.requires_grad = False
 
-        for param in self.net.classifier.parameters():
+        for param in self.classifier_layers():
             param.requires_grad = True
 
 
-class AlexNet(nn.Module):
+class AlexNet(MaskBaseModel):
 
-    def __init__(self, num_classes=3, pretrained=False, freeze=False):
-        super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
+    def build_layers(self, num_classes, pretrained):
+        from torchvision.models import alexnet
+        net = alexnet(pretrained=pretrained)
+        net.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
@@ -68,38 +63,20 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
+        return net
 
-        if not pretrained:
-            self._initialize_weights()
+    def classifier_layers(self):
+        """ returns classifier layers to be referenced for weight freezing or lr targeting """
 
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.xavier_uniform_(m.weight.data)
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+        return self.net.classifier
 
 
-class VGG19(nn.Module):
+class VGG19(MaskBaseModel):
 
-    def __init__(self, num_classes=3, pretrained=False, freeze=False):
-        super().__init__()
-        from torchvision.models import vgg19
-        self.net = vgg19(pretrained=pretrained)
-        self.net.classifier = nn.Sequential(
+    def build_layers(self, num_classes, pretrained):
+        from torchvision.models import vgg19_bn
+        net = vgg19_bn(pretrained=pretrained)
+        net.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(True),
             nn.Dropout(),
@@ -108,32 +85,23 @@ class VGG19(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, num_classes),
         )
+        return net
 
-        if not pretrained:
-            self._initialize_weights()
+    def classifier_layers(self):
+        """ returns classifier layers to be referenced for weight freezing or lr targeting """
 
-        if freeze:
-            self._freeze_net()
+        return self.net.classifier
 
-    def forward(self, x):
-        return self.net(x)
 
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.xavier_uniform_(m.weight.data)
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+class Resnet18(MaskBaseModel):
 
-    def _freeze_net(self):
-        for param in self.net.parameters():
-            param.requires_grad = False
+    def build_layers(self, num_classes, pretrained):
+        from torchvision.models import resnet18
+        net = resnet18(pretrained=pretrained)
+        net.fc = nn.Linear(512, num_classes)
+        return net
 
-        for param in self.net.classifier.parameters():
-            param.requires_grad = True
+    def classifier_layers(self):
+        """ returns classifier layers to be referenced for weight freezing or lr targeting """
+
+        return nn.Sequential(self.net.fc)
