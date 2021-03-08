0d031dcdf9f7b1b4ce69a304f82e55cb41f73d02
MoMentum99 <kmc2048@gmail.com>
2021-03-09 03:37:30 +0900

    Remove pretrained models

--- model.py
+++ model.py
@@ -1,107 +1,40 @@
 import torch
 import torch.nn as nn
-import torch.nn.init as init
+import torch.nn.functional as F
 
 
-class MaskBaseModel(nn.Module):
-    def __init__(self, num_classes=3, pretrained=False, freeze=False):
+class BaseModel(nn.Module):
+    def __init__(self, num_classes):
         super().__init__()
 
-        self.net = self.build_layers(num_classes, pretrained)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=7, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.25)
+        self.dropout3 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(77952, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
-        self._initialize_weights(pretrained)
-
-        if freeze:
-            self._freeze_net()
-
-    def build_layers(self, num_classes, pretrained):
-        raise NotImplementedError
-
-    def classifier_layers(self):
-        """ returns classifier layers to be referenced for weight freezing or weight initializing """
-
-        raise NotImplementedError
-
+    # recpetive field: 13
+    # https://fomoro.com/research/article/receptive-field-calculator#7,1,1,VALID;3,2,1,VALID;3,2,1,VALID
     def forward(self, x):
-        return self.net(x)
+        x = self.conv1(x)
+        x = F.relu(x)
 
-    def _initialize_weights(self, pretrained):
-        """ initialize all weights if not pretrained, otherwise initialize only classifier weights """
-        modules = self.modules() if not pretrained else self.classifier_layers()
-        for m in modules:
-            if isinstance(m, nn.Conv2d):
-                init.xavier_uniform_(m.weight.data)
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
 
-    def _freeze_net(self):
-        """ freeze all layers excepting classifier layers """
-        for param in self.net.parameters():
-            param.requires_grad = False
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout2(x)
 
-        for param in self.classifier_layers():
-            param.requires_grad = True
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout3(x)
 
-
-class AlexNet(MaskBaseModel):
-
-    def build_layers(self, num_classes, pretrained):
-        from torchvision.models import alexnet
-        net = alexnet(pretrained=pretrained)
-        net.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-        return net
-
-    def classifier_layers(self):
-        """ returns classifier layers to be referenced for weight freezing or lr targeting """
-
-        return self.net.classifier
-
-
-class VGG19(MaskBaseModel):
-
-    def build_layers(self, num_classes, pretrained):
-        from torchvision.models import vgg19_bn
-        net = vgg19_bn(pretrained=pretrained)
-        net.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-        return net
-
-    def classifier_layers(self):
-        """ returns classifier layers to be referenced for weight freezing or lr targeting """
-
-        return self.net.classifier
-
-
-class Resnet18(MaskBaseModel):
-
-    def build_layers(self, num_classes, pretrained):
-        from torchvision.models import resnet18
-        net = resnet18(pretrained=pretrained)
-        net.fc = nn.Linear(512, num_classes)
-        return net
-
-    def classifier_layers(self):
-        """ returns classifier layers to be referenced for weight freezing or lr targeting """
-
-        return nn.Sequential(self.net.fc)
+        return self.fc2(x)
