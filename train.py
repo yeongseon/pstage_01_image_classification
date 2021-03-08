ac2acf0960c638e3dd0b3acb4c13d2bb04bde3d1
MoMentum99 <kmc2048@gmail.com>
2021-03-09 04:10:09 +0900

    Update default criterion: cross_entropy

--- train.py
+++ train.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
     parser.add_argument('--momentum', type=float, default=0.5, help='optimizer momentum (default: 0.5)')
     parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
-    parser.add_argument('--criterion', type=str, default='label_smoothing', help='criterion type (default: label_smoothing)')
+    parser.add_argument('--criterion', type=str, default='cross_entropy', help='criterion type (default: cross_entropy)')
     parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
     parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
     parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
