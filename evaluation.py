f060874e97343cf19d052cada44f80d0febb5ef0
JunYeopLee <junyeop.lee@upstage.ai>
2021-03-22 13:28:04 +0900

    update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,4 @@
+import json
 import argparse
 import os
 from importlib import import_module
@@ -13,37 +14,32 @@ def evaluation(gt_dir, pred_dir):
         gt_dir (string) : root directory of ground truth file
         pred_dir (string) : root directory of prediction file (output of inference.py)
     """
-    num_classes = getattr(import_module("dataset"), args.dataset).num_classes
+    num_classes = 18
     results = {}
 
     gt = pd.read_csv(os.path.join(gt_dir, 'gt.csv'))
     pred = pd.read_csv(os.path.join(pred_dir, 'output.csv'))
-    cls_report = classification_report(gt.ans.values, pred.ans.values, labels=np.arange(num_classes), output_dict=True)
-    acc = round(cls_report['accuracy'] * 100, 2)
+    cls_report = classification_report(gt.ans.values, pred.ans.values, labels=np.arange(num_classes), output_dict=True, zero_division=0)
+    acc = cls_report['accuracy'] * 100
     f1 = np.mean([cls_report[str(i)]['f1-score'] for i in range(num_classes)])
-    f1 = round(f1, 2)
 
     results['accuracy'] = {
-        'value': acc,
+        'value': f'{acc:.2f}%',
         'rank': True,
         'decs': True,
     }
     results['f1'] = {
-        'value': f1,
+        'value': f'{f1:.2f}%',
         'rank': False,
         'decs': True,
     }
 
     return json.dumps(results)
 
-
 #if __name__ == '__main__':
-#    parser = argparse.ArgumentParser()
-#    parser.add_argument('--dataset', type=str, default='MaskMultiClassDataset', help='dataset type (default: MaskMultiClassDataset)')
-#    args = parser.parse_args()
-#
 #    gt_dir = os.environ.get('SM_GROUND_TRUTH_DIR')
 #    pred_dir = os.environ.get('SM_OUTPUT_DATA_DIR')
 #
-#    result_str = evaluation(gt_dir, pred_dir)
-#    print(f'Final Score: {result_str}')
+#    from pprint import pprint
+#    results = evaluation(gt_dir, pred_dir)
+#    pprint(results)
